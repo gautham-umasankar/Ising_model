f97ebdbe420213e6a33529575158c7835c5d6c46
parsidd <ee17b059@smail.iitm.ac.in>
2020-03-09 17:01:13 +0530

    First try on max cut problem

---
+++ Max_Cut/mc_try1.py
@@ -0,0 +1,114 @@
+#max cut in ising model
+
+import numpy as np
+from numpy.random import rand
+import matplotlib.pyplot as plt
+import time
+
+
+niter=50    # Number of times we average over
+N=100         # Problem Size
+temp=30     # Temperature
+nmoves=50   # Number of MC moves to equilibriate
+nmax=1000   # Max number allowed
+
+class Ising():
+
+    def __init__(self,N,amp):
+        self.N=N
+        self.amp=amp
+        self.E0=0
+        self.E=0
+        self.v=2*np.random.randint(0,2,N)-1
+        j=np.where(self.v<0)
+        self.amp[j,:]=-self.amp[j,:]
+        self.amp[:,j]=-self.amp[:,j]
+        self.cut=0
+        self.initEnergy()      
+        
+    def initEnergy(self):
+        #Calculates Initial Energy
+        print(self.amp)
+        ii=np.where(self.amp<0)
+        print(ii)
+        self.cut=sum(self.amp[ii])/2
+        print("Initial cut=",self.cut)
+        
+
+    def calcenergy(self,a):
+        #Calculates difference in Energy if spin at a is flipped, i.e. the node a is transferred to the other set
+        e=-sum(self.amp[a])
+        return e
+
+    def mcmove(self,beta):
+        '''Makes N monte carlo moves'''
+        for i in range(N):
+            #print(self.amp)
+            a = np.random.randint(0, self.N) #Picking Random node
+            #print("Node {} selected".format(a))
+            s=self.v[a]
+            
+            cost=self.calcenergy(a)
+
+            #Flipping according to MC algorithm
+            if cost < 0:	
+                #print("Flip due to reduction in energy")
+                s *= -1
+                self.amp[a,:]=-self.amp[a,:]
+                self.amp[:,a]=-self.amp[:,a]
+                self.cut+=cost
+            elif rand() < np.exp(-cost*beta):
+                #print("Flip due to thermal energy")
+                s *= -1
+                self.amp[a,:]=-self.amp[a,:]
+                self.amp[:,a]=-self.amp[:,a]
+                self.cut+=cost
+
+            self.v[a] = s
+
+    def simulate(self,temp,nmoves):   
+        '''Runs nmoves no. of iterations at temp'''
+        for i in range(nmoves):
+            self.mcmove(1.0/temp)
+
+        """Ef=0        
+        while(abs(self.E-Ef)!=0): #Stop running when energy doesn't change. Other bounds can be used
+            if self.E==0:
+                break
+            Ef=self.E
+            self.mcmove(1.0/temp)"""
+
+l=np.random.randint(nmax,size=(N,N)) #Problem Instance
+l=l+l.T
+
+for i in range(N):l[i,i]=0;
+
+rm=Ising(N,l)
+#print("Initial graph=\n",rm.amp)
+rm.simulate(temp,nmoves)
+print(rm.amp)
+print("Final cut=",rm.cut)
+
+
+"""fptr=open("energies.txt",'w')
+
+for N in range(50,55):
+    l=np.random.randint(nmax,size=(N,N)) #Problem Instance
+    t=0     #Time taken
+    En=0    #Final Energy 
+    ti=0    #Dummy variables for time measurement
+    tf=0
+    for i in range(niter): #We do repeated iterations of the same instance and average over it
+        ti=time.time()
+        rm=Ising(N,l)
+        rm.initEnergy()
+        rm.simulate(temp,nmoves)
+        tf=time.time()
+        t=t+tf-ti
+        En=En+rm.E
+    t=t/niter
+    En=En/niter    
+    fptr.write("{} {} {}\n".format(N,En,t))
+
+
+fptr.close()"""
