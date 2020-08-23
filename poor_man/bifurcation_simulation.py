19bf93af3baaad00b1a45d99ad1d7f04e12b1ab5
parsidd <ee17b059@smail.iitm.ac.in>
2020-08-24 00:01:39 +0530

    Simulation of our experiment. Check why the saturation condition didn't work)

---
+++ poor_man/bifurcation_simulation.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+N = 10
+N_alpha = 1000
+
+def modulator(x):
+    return pow(np.cos(x - 0.25*np.pi),2) - 1/2
+
+
+Alpha = np.linspace(0,3,N_alpha)
+beta = 0.0
+final_x = np.zeros([N,1])
+
+for alpha in Alpha:
+    x_k = np.ones([N,1])
+    x_n = np.zeros([N,1])
+    old_x_k = x_k
+    print("Working with Alpha = ", alpha,"...")
+    i = 0
+    while(i < 100):
+        x_k = x_n
+        noise = np.random.normal(0,0.004,[N,1])
+        # put the coupled equation instead
+        x_n = modulator(alpha*x_k + noise)
+        # print("x_n = ",x_n)
+        i+=1
+
+    print("x_n = ",x_n)
+    print("__________")
+    final_x = np.c_[final_x,x_n]
+
+plt.plot(Alpha,final_x[:,1:].T,"r.",markersize=2)
+plt.show()
