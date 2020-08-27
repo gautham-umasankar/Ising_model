ebd6ed5b8208150df3ba122a9b34358f384c701e
gautham-umasankar <gauth2511@gmail.com>
2020-08-27 00:14:52 +0000

    More timing data

--- poor_man/timing.c
+++ poor_man/timing.c
@@ -6,11 +6,12 @@ void main()
 {
 	float i,j,s;
 	int N, k;
-	N = 10000;
+	N = 20000;
 	clock_t start,end;
 	double cpu_time;
-	for(k=1;k<N;k++)
+	for(k=10000;k<N;k+=1000)
 	{
+		//printf("The value of k is : %d\n",k);
 		start = clock();
 		for(i=1.0;i<=k;i+=1.0)
 		{
@@ -23,5 +24,6 @@ void main()
 		cpu_time = ((double)(end - start))/CLOCKS_PER_SEC;
 		printf("%d %lf \n",k,cpu_time);
 	}
+	
 	
 }
