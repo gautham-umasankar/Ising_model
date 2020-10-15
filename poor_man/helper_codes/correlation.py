d361c7e4279e368cd88996c0d8fd93327376b08a
parsidd <ee17b059@smail.iitm.ac.in>
2020-10-15 17:10:33 +0530

    Added correlation code for window sweeping over one buffer.

--- poor_man/helper_codes/correlation.py
+++ poor_man/helper_codes/correlation.py
@@ -7,29 +7,56 @@ class correlation_calc:
     def __init__(self, file):
         self.f = open(data_file)
     
-    def calc(self, shifts = np.array(range(-60,60,1))):
+    def correlate(self, buff_in, buff_out, shifts):
+        return np.array([sum(buff_in*np.roll(buff_out,shift)) for shift in shifts])/(np.sqrt(sum(buff_out*buff_out))*np.sqrt(sum(buff_in*buff_in)))
+    
+    def calc(self, iteration = -1, shifts = np.array(range(-60,60,1))):
         lines = self.f.readlines()[2:]
         self.f.close()
         buff_size = 16384
+        window_size = 2048
         iters = int(len(lines)/buff_size)
 
         x_in = np.array([float(i.split()[3]) for i in lines])
         x_out = np.array([float(i.split()[2]) for i in lines])
         correlations = []
         for i in range(iters):
-            fig = plt.figure(i)
-            corr = np.array([sum(x_in[i*buff_size:(i+1)*buff_size]*np.roll(x_out[i*buff_size:(i+1)*buff_size],shift)) for shift in range(-60,60,1)])/(sum(x_out[i*buff_size:(i+1)*buff_size]))
-            correlations.append(np.argmax(corr))
-            plt.title("Iteration: {}".format(i+1))
-            plt.plot(shifts,corr)
+            # fig = plt.figure(i)
+            buff_out = x_out[i*buff_size:(i+1)*buff_size]
+            buff_in = x_in[i*buff_size:(i+1)*buff_size]
+            corr = self.correlate(buff_in, buff_out, shifts)
+            correlations.append(shifts[np.argmax(corr)])
+            # plt.title("Iteration: {}".format(i+1))
+            # plt.plot(shifts,corr)
+            if(i == iteration):
+                corrs = []
+                fig = plt.figure()
+                # plt.plot(buff_out)
+                # plt.plot(buff_in)
+                # plt.show()
+                for j in range(int(buff_size/window_size)):
+                    # fig = plt.figure(j)
+                    shifts_2 = np.array(range(-20,20,1))
+                    t_out = buff_out[j*window_size:(j+1)*window_size]
+                    t_in = buff_in[j*window_size:(j+1)*window_size]
+                    c = self.correlate(t_in, t_out, shifts_2)
+                    corrs.append(shifts_2[np.argmax(c)])
+                    # plt.title("Window: {}".format(j+1))
+                    plt.plot(shifts_2,c, label = "Window:{}".format(j))
+                
+                for i,corr in enumerate(corrs):
+                    print("Window {} shift = {}".format(i,corr))
+                plt.legend()
+                plt.show()
 
-        plt.show()
+        # plt.show()
 
         for i,corr in enumerate(correlations):
             print("Iteration {} shift = {}".format(i,corr))
 
 if __name__ == "__main__":
     shifts = np.array(range(-60,60,1))
+    iteration = -1
     if(len(sys.argv)>1):
         i = 1
         while(i <= len(sys.argv[1:])):
@@ -41,6 +68,8 @@ if __name__ == "__main__":
                 data_file = str(sys.argv[i+1])
             elif(opt == '-shift'):
                 shifts = float(sys.argv[i+1])
+            elif(opt == "-iteration"):
+                iteration = int(sys.argv[i+1])
             '''elif(opt == '-da'):
                 min_alpha = float(sys.argv[i+1])
             elif(opt == '-sa'):
@@ -51,4 +80,4 @@ if __name__ == "__main__":
                 plot_alpha = min_alpha'''
             i+=1
     c = correlation_calc(data_file)
-    c.calc(shifts)
+    c.calc(iteration, shifts)
